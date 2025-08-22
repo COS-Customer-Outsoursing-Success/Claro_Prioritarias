0dfd706e5a74e341b88f5213e1c8c1b19776f9c6
emersonaguilar0694 <emerson.aguilar@groupcosbpo.com>
2025-08-22 14:15:35 -0500

    actualizacion config formulario and depurador predictivo falabella

--- sql/_sql_depurador_predictivo_falabella.sql
+++ sql/_sql_depurador_predictivo_falabella.sql
@@ -1,65 +1,74 @@
-with base as (
-    select *,
-        case 
-            when excluir = 1
-			OR placa IN ( SELECT placa FROM bbdd_cos_bog_grupo_axa.tb_asignacion_falabella_v2_no_aptos WHERE periodo IN (
-            202508
-				)
-            )
-            then 1 
-            else 0 
-        end as exclusiones_general
-    from bbdd_cos_bog_grupo_axa.tb_asignacion_falabella_v2_coalesce
-    where 
-    periodo IN (
-    202508
-    )
-),
-consolidados as (
-    select *,
-        max(exclusiones_general) over(partition by placa) as exclusion_total,
-        row_number() over(partition by placa order by fecha_ultima_gestion asc) as orden
-    from base
+WITH base AS (
+    SELECT *,
+        CASE 
+            WHEN excluir = 1
+              OR placa IN (
+                  SELECT placa 
+                  FROM bbdd_cos_bog_grupo_axa.tb_asignacion_falabella_v2_no_aptos 
+                  WHERE periodo = 202508
+              )
+            THEN 1 ELSE 0 
+        END AS exclusiones_general
+    FROM bbdd_cos_bog_grupo_axa.tb_asignacion_falabella_v2_coalesce
+    WHERE periodo = 202508
 )
-
+, consolidados AS (
+    SELECT *,
+           MAX(exclusiones_general) OVER(PARTITION BY placa) AS exclusion_total
+    FROM base
+)
 SELECT 
 *
 FROM consolidados
-WHERE 1 = 1
-	AND orden = 1
-	AND exclusion_total = 0
-    
--- Seleccion de telefonos a marcar    
-	AND tipo_phone IN (
-    'telefono1'
-#   'telefono2'
-#   'telefono3'
-	)
-    
--- Seleccion de cantidad de intentos a marcar
-	AND (
-		vicidial_calls <= 0
-		OR vicidial_calls IS NULL
-	)
-        
--- Eleccion si se marca no contacto y sin gestion
-	AND (
-        tipificacion_mejor_gestion_soul IS NULL
-        OR tipo_mejor_gestion IS NULL
-        
-#        OR tipo_mejor_gestion = 'No Contacto'
-#        OR tipificacion_mejor_gestion_soul IN ('No Contestan', 'Grabadora o Buzon', 'Cliente Cuelga La Llamada')
-	)
+WHERE exclusion_total = 0
 
--- De aca para abajo no cambia, solo en casos especiales
+-- ---------------------------------------------------------------------------- --
+-- Sin gestion : Descomentar colocando un # al inicio de los simbolos "/*" ---- --
+-- ---------------------------------------------------------------------------- --
+
+/*
+	AND tipo_phone = 'telefono1'
+	AND (vicidial_calls = 0 OR vicidial_calls IS NULL)
+*/
+
+-- ---------------------------------------------------------------------------- --
+-- No Contacto : Descomentar colocando un # al inicio de los simbolos "/*" ---- --
+-- ---------------------------------------------------------------------------- --
+
+/*
+    AND vicidial_calls <= 1
+	AND (tipo_mejor_gestion = 'No Contacto'
+	OR tipificacion_mejor_gestion_soul IN ('No Contestan', 'Grabadora o Buzon', 'Cliente Cuelga La Llamada')
+    )
+*/  
+
+-- ---------------------------------------------------------------------------- --
+-- Seguimientos: Descomentar colocando un # al inicio de los simbolos "/*" ---- --
+-- ---------------------------------------------------------------------------- --
+
+/*
+    AND vicidial_calls <= 1
+	AND (tipificacion_mejor_gestion_soul IN ('Volver a llamar')
+    )
+*/  
+
+-- ---------------------------------------------------------------------------- --
+-- Fechas De Vigencia: Descomentar colocando un # al inicio de los simbolos "/*" -
+-- ---------------------------------------------------------------------------- --
+
+/*
 	AND DATE_FORMAT(fecha_fin_vigencia_actual, '%%m%%d') 
 	BETWEEN  DATE_FORMAT(CURDATE() + INTERVAL 1 DAY, '%%m%%d') AND DATE_FORMAT(CURDATE() + INTERVAL 15 DAY, '%%m%%d')
-	
+*/
+-- ---------------------------------------------------------------------------- --
+-- No comentar esta parte, evita que se marquen registros marcados entre hoy y ayer
+-- ---------------------------------------------------------------------------- --
+
     AND (
-		fecha_ultima_gestion_soul IS NULL OR DATE(fecha_ultima_gestion_soul) < CURDATE() - INTERVAL 1 DAY
+		fecha_ultima_gestion IS NULL OR DATE(fecha_ultima_gestion) < CURDATE() - INTERVAL 1 DAY
 	)
 	AND (
 		fecha_ultima_gestion IS NULL OR DATE(fecha_ultima_gestion) < CURDATE() - INTERVAL 1 DAY
 	)
-    
-ORDER BY  fecha_asignacion DESC, vicidial_calls asc, tipificacion_ultima_gestion_soul ASC, tipo_phone asc
+
+ORDER BY fecha_asignacion DESC, vicidial_calls ASC, tipo_phone ASC;
